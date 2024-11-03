6cad90fcef57ee499d06b5ac764eeea327c92006
Mahnoor Ashfaq <mahnoorashfaq474@gmail.com>
2024-11-03 22:19:17 +0500

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    "rules": {
+      "react/no-unescaped-entities": "off"
+    }
+};
 
 export default nextConfig;
+  
